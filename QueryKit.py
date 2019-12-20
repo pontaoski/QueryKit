a311aba6b57bdeda6cfde1461d42674a80f5f595
Carson Black <uhhadd@gmail.com>
2019-12-19 19:21:25 -0500

    Repoquery

--- QueryKit.py
+++ QueryKit.py
@@ -31,6 +31,16 @@ class QueryKit(object):
               <arg type='s' name='distro' direction='in'/>
               <arg type='a(sssii)' name='packages' direction='out'/>
             </method>
+            <method name='QueryRepo'>
+              <arg type='a{ss}' name='queries' direction='in'/>
+              <arg type='s' name='distro' direction='in'/>
+              <arg type='a(sssii)' name='packages' direction='out'/>
+            </method>
+            <method name='ListFiles'>
+              <arg type='s' name='package' direction='in'/>
+              <arg type='s' name='distro' direction='in'/>
+              <arg type='as' name='files' direction='out'/>
+            </method>
             <property name="Distros" type="as" access="read">
               <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="false"/>
             </property>
@@ -47,6 +57,54 @@ class QueryKit(object):
         dnf_query_obj: dnf.query.Query = self._dnf_objects[distro].sack.query()
         available_pkgs: dnf.query.Query = dnf_query_obj.available()
         available_pkgs: dnf.query.Query = available_pkgs.filter(name__substr=query,arch=["noarch","x86_64"])
+
+        pkgs = []
+        for pkg in available_pkgs:
+            pkgs.append((pkg.name, pkg.summary, pkg.version, pkg.downloadsize, pkg.installsize))
+
+        return pkgs
+
+    def ListFiles(self, package, distro):
+        if distro not in self._dnf_objects.keys():
+            return ["Invalid distro."]
+        dnf_query_obj: dnf.query.Query = self._dnf_objects[distro].sack.query()
+        available_pkgs: dnf.query.Query = dnf_query_obj.available()
+
+        available_pkgs.filterm(name__substr=package, arch=["noarch","x86_64"])
+
+        if available_pkgs[0] is None:
+            return ["No package found."]
+
+        return available_pkgs[0].files
+
+    def QueryRepo(self, queries, distro):
+        if distro not in self._dnf_objects.keys():
+            return [('Invalid Distro', 'This is an invalid distro.','N/A', -1, -1)]
+        dnf_query_obj: dnf.query.Query = self._dnf_objects[distro].sack.query()
+        available_pkgs: dnf.query.Query = dnf_query_obj.available()
+
+        available_pkgs.filterm(arch=["noarch","x86_64"])
+
+        if "file" in queries.keys():
+            available_pkgs.filterm(file__glob=queries["file"])
+        if "whatconflicts" in queries.keys():
+            available_pkgs.filterm(conflicts=queries["whatconflicts"])
+        if "whatobsoletes" in queries.keys():
+            available_pkgs.filterm(obsoletes=queries["whatobsoletes"])
+        if "whatprovides" in queries.keys():
+            q_f_p = available_pkgs.filterm(provides__glob=queries["whatprovides"])
+            if q_f_p:
+                available_pkgs = q_f_p
+            else:
+                available_pkgs.filterm(file__glob=queries["whatprovides"])
+        if "whatrecommends" in queries.keys():
+            available_pkgs.filterm(recommends__glob=queries["whatrecommends"])
+        if "whatenhances" in queries.keys():
+            available_pkgs.filterm(enhanced__glob=queries["whatenhances"])
+        if "whatsupplements" in queries.keys():
+            available_pkgs.filterm(supplements__glob=queries["whatsupplements"])
+        if "whatsuggests" in queries.keys():
+            available_pkgs.filterm(suggests__glob=queries["whatsuggests"])
 
         pkgs = []
         for pkg in available_pkgs:
@@ -71,6 +129,7 @@ class QueryKit(object):
             except:
                 print("Could not refresh {}.".format(key))
             print("Refreshed {}!".format(key))
+        return True
 
     def __init__(self):
         print("Loading repos...")
@@ -85,7 +144,7 @@ class QueryKit(object):
                 self._dnf_objects[key].conf.substitutions['arch'] = arch
                 self._dnf_objects[key].conf.substitutions['basearch'] = dnf.rpm.basearch(arch)
                 if key == "fedora":
-                    self._dnf_objects[key].conf.substitutions['releasever'] = '30'
+                    self._dnf_objects[key].conf.substitutions['releasever'] = '31'
                 if key == "openmandriva":
                     self._dnf_objects[key].conf.substitutions['releasever'] = '4.0'
                 if key == "mageia":
@@ -105,7 +164,7 @@ class QueryKit(object):
         for i in to_pop:
             self._dnf_objects.pop(i)
 
-        GLib.timeout_add_seconds(60, self.RefreshPackages)
+        GLib.timeout_add_seconds(1800, self.RefreshPackages)
         print("Repos loaded!")
 
 loop = GLib.MainLoop()
